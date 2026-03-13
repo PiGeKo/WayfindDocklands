ebd95a891c99b5419f8e4435b1ffe379173c557b
PiGeKo <piotrgkonkol@gmail.com>
2026-03-13 22:21:29 +0000

    Initialising project to GitHub

---
+++ Wayfind/src/main/java/com/learning/wayfind/LaunchApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.learning.wayfind;
+
+/**
+ *
+ * @author Konko
+ */
+public class LaunchApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
